9eb9549381c7fe1ff8a847c3d3164a06a87c268e
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-01-04 18:21:47 +0100

    Created UserDaoService
    added some basic methods

---
+++ src/main/java/com/freann/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,38 @@
+package com.freann.rest.webservices.restfulwebservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCounter = 3;
+
+    static {
+        users.add(new User(1, "Mario", new Date()));
+        users.add(new User(2, "Luigi", new Date()));
+        users.add(new User(3, "Wario", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User saveUser(User user) {
+        if (user.getId() == null) {
+            user.setId(++usersCounter);
+        }
+        users.add(user);
+        return user;
+    }
+
+    public User fundById(Integer id) {
+        return users.stream().filter(user -> Objects.equals(user.getId(), id)).findFirst().orElse(null);
+    }
+}
