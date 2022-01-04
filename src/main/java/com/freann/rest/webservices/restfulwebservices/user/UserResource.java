4458793e47fa321da160740e22fcbc50118584c3
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-01-04 19:46:20 +0100

    added create user post mapping
    also added in a postman collection

--- src/main/java/com/freann/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/freann/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,10 +1,7 @@
 package com.freann.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,9 @@ public class UserResource {
         return this.userDaoService.findById(id);
     }
 
-
+    @PostMapping
+    public void createUser(@RequestBody User user){
+        this.userDaoService.saveUser(user);
+    }
 
 }
