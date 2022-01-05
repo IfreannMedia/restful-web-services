d7b387305ffc13ab7ded2076d92fcb426ddf41b2
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-01-05 19:48:20 +0100

    Added basic i18n
    and implemented in one endpoint, updated postman

--- src/main/java/com/freann/rest/webservices/restfulwebservices/helloworld/controllers/HelloWorldController.java
+++ src/main/java/com/freann/rest/webservices/restfulwebservices/helloworld/controllers/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.freann.rest.webservices.restfulwebservices.helloworld.controllers;
 
 import com.freann.rest.webservices.restfulwebservices.helloworld.beans.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -21,5 +29,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-var/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("hello-world, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldInternationalised(@RequestHeader(name = "accept-language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 }
