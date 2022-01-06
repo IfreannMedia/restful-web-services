c5f9c3249d434ae2f95b6c8a54da9af69dc53d9f
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-01-06 16:09:05 +0100

    Implemented dynamic property filtering
    using jackson, updated postman

--- src/main/java/com/freann/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/freann/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,5 +1,9 @@
 package com.freann.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +14,29 @@ import java.util.List;
 public class FilteringController {
 
     @GetMapping("/filtering")
-    public SomeBean retrieveSomeBean() {
-        return new SomeBean("Value 1", "Value 2", "Sensitive Value 3");
+    public MappingJacksonValue retrieveSomeBean() {
+        SomeBean bean = new SomeBean("Value 1", "Value 2", "Sensitive Value 3");
+
+        return getMappingJacksonValue(bean, "SomeBeanFilter", SimpleBeanPropertyFilter
+                .filterOutAllExcept("field1", "field2"));
+    }
+
+    private MappingJacksonValue getMappingJacksonValue(Object bean, String filterId, SimpleBeanPropertyFilter propFilter) {
+        MappingJacksonValue mapping = new MappingJacksonValue(bean);
+        FilterProvider filters = new SimpleFilterProvider().addFilter(filterId, propFilter);
+        mapping.setFilters(filters);
+        return mapping;
     }
 
     @GetMapping("/filtering/all")
-    public List<SomeBean> retrieveListOfSomeBean() {
-        return Arrays.asList(
+    public MappingJacksonValue retrieveListOfSomeBean() {
+        List<SomeBean> someBeans = Arrays.asList(
                 new SomeBean("Value 1", "Value 2", "Sensitive Value 3"),
                 new SomeBean("Value Unos", "Value Dos", "Sensitive Value Tres"),
                 new SomeBean("Value Eins", "Value Zwei", "Sensitive Value Drei")
         );
+
+        return getMappingJacksonValue(someBeans, "SomeBeanFilter", SimpleBeanPropertyFilter
+                .filterOutAllExcept("field3"));
     }
 }
